cf2ea5b4a36a921b4a5dc14207099cb74b067de8
Sergio Rodríguez González <srg301091@hotmail.com>
2020-10-17 15:46:26 -0500

    1. Clean up the code a bit 2. Add multiplication and division (e.g., 2*3) 3. Add the ability to handle more than one operand (e.g., 1+2+3)

--- main.cpp
+++ main.cpp
@@ -2,18 +2,35 @@
 
 int main()
 {
-	cout << "Please enter expression (we can handle + and -) : ";
+	cout << "Please enter expression (we can handle +, -, *, and /) : ";
 	int lval = 0;
 	int rval = 0;
 	char op = '+';
 	int res = 0;
-	cin >> lval >> op >> rval; // read something like 1 + 3
-	if (op == '+')
-		res = lval + rval; // addition
-	else if (op == '-')
-		res = lval - rval; // subtraction
+	cin >> lval; // read leftmost operand
+	if (!cin) error("no first operand");
 	
-	cout << "Result: " << res << '\n';
-	keep_window_open();
-	return 0;
+	while (cin >> op) { // read operator and right-hand operand repeatedly 
+		cin >> rval;
+		if (!cin) error("no second operand");
+		switch (op) {
+		case '+':
+			lval += rval; // add: lval = lval + rval
+			break;
+		case '-':
+			lval -= rval; // subtract: lval = lval - rval
+			break;
+		case '*':
+			lval *= rval; // multiply: lval = lval * rval
+			break;
+		case '/':
+			lval /= rval; // divide: lval = lval / rval
+			break;
+		default:
+			cout << "Result: " << lval << '\n'; // not another operator: print result
+			keep_window_open();
+			return 0;
+		}
+	}	
+	error("bad operation");
 }
